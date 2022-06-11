e4a273bb5d17a67ac9db980fa0a30125ed9929be
Pierre Grimaud <grimaud.pierre@gmail.com>
2022-06-11 17:33:03 +0200

    Add token transfer

--- src/Worker/Client/ImmutableXClient.php
+++ src/Worker/Client/ImmutableXClient.php
@@ -19,13 +19,14 @@ class ImmutableXClient
     )
     {
         $this->httpClient = HttpClient::create();
+        /** @todo refactor transfers and test with asset contract */
     }
 
-    public function transferNft(array $payload): void
+    public function transferNft(array $payload): array
     {
         $response = $this->httpClient->request('POST', 'https://api.x.immutable.com/v1/signable-transfer-details', [
             'json' => [
-                'amount' => "1",
+                'amount' => '1',
                 'receiver' => $payload['recipient'],
                 'sender' => $this->publicKey,
                 'token' => [
@@ -65,13 +66,52 @@ class ImmutableXClient
             ],
         ]);
 
-        $result = $response->toArray();
-        dd($result);
+        return $response->toArray();
     }
 
-    public function transferCrypto(array $payload): void
+    public function transferToken(array $payload): array
     {
-        /* @todo do it bro */
-        unset($payload);
+        $response = $this->httpClient->request('POST', 'https://api.x.immutable.com/v1/signable-transfer-details', [
+            'json' => [
+                'amount' => $payload['token']['quantity'],
+                'receiver' => $payload['recipient'],
+                'sender' => $this->publicKey,
+                'token' => [
+                    'data' => [
+                        'decimals' => $payload['token']['decimals'],
+                    ],
+                    'type' => 'ETH',
+                ]
+            ],
+        ]);
+
+        $resultSignable = $response->toArray();
+
+        $starkNetSigner = new StarkSigner($this->publicKey, $this->privateKey);
+        $starkSignature = $starkNetSigner->signMessage($resultSignable['payload_hash']);
+
+        $ethereumSigner = new EthereumSigner($this->privateKey);
+
+        $response = $this->httpClient->request('POST', 'https://api.x.immutable.com/v1/transfers', [
+            'headers' => [
+                'x-imx-eth-address' => $this->publicKey,
+                'x-imx-eth-signature' => $ethereumSigner->signMessage(
+                    $resultSignable['signable_message'],
+                ),
+            ],
+            'json' => [
+                'amount' => $resultSignable['amount'],
+                'asset_id' => $resultSignable['asset_id'],
+                'expiration_timestamp' => $resultSignable['expiration_timestamp'],
+                'nonce' => $resultSignable['nonce'],
+                'receiver_stark_key' => $resultSignable['receiver_stark_key'],
+                'receiver_vault_id' => $resultSignable['receiver_vault_id'],
+                'sender_stark_key' => $resultSignable['sender_stark_key'],
+                'sender_vault_id' => $resultSignable['sender_vault_id'],
+                'stark_signature' => $starkSignature,
+            ],
+        ]);
+
+        return $response->toArray();
     }
 }
--- src/Worker/Command/RunCommand.php
+++ src/Worker/Command/RunCommand.php
@@ -8,6 +8,7 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Worker\Client\ImmutableXClient;
@@ -18,12 +19,17 @@ class RunCommand extends Command
 {
     const ALLOWED_TASKS = [
         'task-transfer-nft',
-        'task-transfer-crypto',
+        'task-transfer-token',
     ];
+
+    const STATUS_OK = 'OK';
+    const STATUS_KO = 'KO';
 
     private HttpClientInterface $httpClient;
     private readonly array $credentials;
     private readonly ImmutableXClient $immutableXClient;
+
+    private SymfonyStyle $io;
 
     public function __construct()
     {
@@ -45,13 +51,14 @@ class RunCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->fetchMessage();
+        $this->io = new SymfonyStyle($input, $output);
 
-        return Command::SUCCESS;
+        return $this->runProcess();
     }
 
-    private function fetchMessage(): void
+    private function runProcess(): int
     {
+        // fetch message
         $response = $this->httpClient->request('GET', $this->credentials['api_url'] . '/messages', [
             'headers' => [
                 'x-api-key' => $this->credentials['api_key'],
@@ -61,21 +68,72 @@ class RunCommand extends Command
         $result = $response->toArray();
 
         if (!isset($result['message'])) {
-            /** catch error */
-            throw new \Exception('Error todo');
+            // nothing to do
+            $this->io->writeln($this->formatLog(sprintf('Nothing to process')));
+
+            // avoid API DDoS
+            sleep(5);
+
+            return Command::SUCCESS;
         } else {
             $message = $result['message'];
         }
 
         if (in_array($message['task'], self::ALLOWED_TASKS)) {
-            switch ($message['task']) {
-                case 'task-transfer-nft':
-                    $this->immutableXClient->transferNft($message['body']);
-                    break;
-                case 'task-transfer-crypto':
-                    $this->immutableXClient->transferCrypto($message['body']);
-                    break;
+            try {
+                switch ($message['task']) {
+                    case 'task-transfer-nft':
+                        $response = $this->immutableXClient->transferNft($message['body']);
+                        break;
+                    case 'task-transfer-token':
+                        $response = $this->immutableXClient->transferToken($message['body']);
+                        break;
+                }
+
+                $this->postResponse($message['id'], (array)$response);
+                return Command::SUCCESS;
+
+            } catch (\Exception $e) {
+                // send errored process
+                $this->postResponse($message['id'], $e->getMessage(), self::STATUS_KO);
+                return Command::FAILURE;
             }
+        } else {
+            // something almost impossible
+            $this->postResponse(
+                $message['id'],
+                sprintf('Unknown task %s', $message['task']),
+                self::STATUS_KO
+            );
+            return Command::FAILURE;
         }
+    }
+
+    private function postResponse(int $id, array|string $response, string $status = self::STATUS_OK): void
+    {
+        $this->httpClient->request('POST', $this->credentials['api_url'] . '/messages', [
+            'headers' => [
+                'x-api-key' => $this->credentials['api_key'],
+            ],
+            'json' => [
+                'message_id' => $id,
+                'response' => [
+                    ($status === self::STATUS_OK ? 'response' : 'error') => $response
+                ],
+                'status' => $status
+            ],
+        ]);
+
+        // log task
+        $this->io->writeln($this->formatLog(sprintf(
+            'Task %s - Status %s',
+            $id,
+            $status
+        )));
+    }
+
+    private function formatLog(string $message): string
+    {
+        return date('Y-m-d H:i:s') . ' ' . $message;
     }
 }
